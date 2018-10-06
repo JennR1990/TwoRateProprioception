191dfeeace142d27a4ffdab610908231493dafdf
thartbm <thartbm@gmail.com>
2018-10-06 11:28:40 -0400

    check/create data directory in downloadOSFdata

--- R/shared.R
+++ R/shared.R
@@ -1,6 +1,7 @@
 
 downloadOSFdata <- function(update=FALSE) {
   
+  # this pulls data from the OSF repo:
   files <- c('active_localization.csv'  = 'https://osf.io/vys7e/?action=download',
              'active_reaches.csv'       = 'https://osf.io/nhcmb/?action=download',
              'nocursor_nocursors.csv'   = 'https://osf.io/b9fc8/?action=download',
@@ -9,6 +10,13 @@ downloadOSFdata <- function(update=FALSE) {
              'passive_reaches.csv'      = 'https://osf.io/mq5av/?action=download',
              'pause_reaches.csv'        = 'https://osf.io/q59b3/?action=download')
   
+  # check if data directory exists and create if necessary:
+  # (data should come from OSF, so is not on github)
+  if (!dir.exists('data')) {
+    dir.create('data')
+  }
+  
+  # check if each file exists and copy it if necessary: 
   for (filename in names(files)) {
     
     filepath <- sprintf('data/%s',filename)
