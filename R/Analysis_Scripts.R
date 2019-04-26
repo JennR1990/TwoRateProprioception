02c9ef6622c512eca485d7001babf5e79d2fff35
Jenn Ruttle <jennruttle@gmail.com>
2019-04-26 15:29:33 -0400

    added plot codes for looking at pre post data

--- R/Analysis_Scripts.R
+++ R/Analysis_Scripts.R
@@ -1,3 +1,31 @@
+###Compare pre- post localization
+#layout(c(1,2,3))
+averagedprepost<- function (dataset = c('Pause', 'NoCursor', 'NewNoC')) {
+  svglite(file='doc/Pre_Post_Data.svg', width=6, height=9, system_fonts=list(sans = "Arial"))
+  layout(c(1,2,3), heights = c(2,2,2))
+
+  exp = list('No-Localization Task','No-Cursor Task', 'New No-Cursor Task')
+  counter<- 1
+  
+  for (data in dataset) {
+    filename<- sprintf('data/%s_pre_post_Prop.csv', data)
+    df<- read.csv(filename, sep = ',', header = TRUE)
+    pre<- mean(unlist(df[56:64, 2:ncol(df)]), na.rm = TRUE)
+    post<- mean(unlist(df[65:73, 2:ncol(df)]), na.rm = TRUE)
+    stuff<- c(pre, post)
+    print(stuff)
+    barplot(stuff, ylim = c(-1.5,1.5), names = c('Pre', 'Post'), col= c('mediumorchid3', 'red'))
+    text(.75,pre+.1, labels = pre)
+    text(2,post+.1, labels = post)
+    title( main = exp[counter] )
+    counter<- counter + 1
+  }
+  dev.off()
+}
+
+
+
+
 
 #regression for error clamp versus whatever you want
 
