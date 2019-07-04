58724f68ec2fb2d78e2dd295f60cc79fa80b0ad3
Jennifer Ruttle <jeruttle@yorku.ca>
2019-07-04 14:16:09 -0400

    removed bootstrap from AIC code

--- R/Analysis_Scripts.R
+++ R/Analysis_Scripts.R
@@ -318,7 +318,7 @@ prepdatagetonefits<- function (data){
   return(pars)
 }
 
-bootstrapModelAICs <- function(data, bootstraps=1, group) {
+bootstrapModelAICs <- function(data, group) {
   #group='active'# add this to the function call when i use the commented line below
   
   library(RateRate)
@@ -327,7 +327,7 @@ bootstrapModelAICs <- function(data, bootstraps=1, group) {
   df<-data
   schedule <- df$distortion
   
-  reaches <- as.matrix(df[,2:dim(df)[2]])
+  Reaches <- as.matrix(df[,2:dim(df)[2]])
   
   N <- dim(df)[2] - 1
   # prep for AICs:
@@ -341,17 +341,17 @@ bootstrapModelAICs <- function(data, bootstraps=1, group) {
   # this is then used for C:
   C <- InOb*(log(2*pi)+1)
   
-  for (bootstrap in c(1:bootstraps)) {
+  #for (bootstrap in c(1:bootstraps)) {
     
-    medReaches <- apply(reaches[,sample(c(1:N),N,replace=TRUE)], 1, median, na.rm=TRUE)
+    #medReaches <- apply(reaches[,sample(c(1:N),N,replace=TRUE)], 1, median, na.rm=TRUE)
     
-    twoRateFit <- fitTwoRateReachModel(reaches=medReaches, schedule=schedule, oneTwoRates=2, grid='restricted', checkStability=TRUE)
-    oneRateFit <- fitTwoRateReachModel(reaches=medReaches, schedule=schedule, oneTwoRates=1, grid='restricted', checkStability=TRUE)
+    twoRateFit <- fitTwoRateReachModel(reaches=Reaches, schedule=schedule, oneTwoRates=2, grid='restricted', checkStability=TRUE)
+    oneRateFit <- fitTwoRateReachModel(reaches=Reaches, schedule=schedule, oneTwoRates=1, grid='restricted', checkStability=TRUE)
     print(oneRateFit)
     print(twoRateFit)
     
-    twoRateMSE <- twoRateReachModelErrors(par=twoRateFit, reaches=medReaches, schedule=schedule)
-    oneRateMSE <- twoRateReachModelErrors(par=oneRateFit, reaches=medReaches, schedule=schedule)
+    twoRateMSE <- twoRateReachModelErrors(par=twoRateFit, reaches=Reaches, schedule=schedule)
+    oneRateMSE <- twoRateReachModelErrors(par=oneRateFit, reaches=Reaches, schedule=schedule)
     
     
     twoRateAIC <- (2*4) + InOb*log(twoRateMSE) + C
@@ -359,7 +359,7 @@ bootstrapModelAICs <- function(data, bootstraps=1, group) {
     
     cat(sprintf('1-rate AIC: %0.2f  %s  2-rate AIC: %0.2f\n',oneRateAIC,c('>=', ' <')[as.numeric(oneRateAIC<twoRateAIC)+1],twoRateAIC))
     
-  }
+  #}
   AICs<- data.frame(twoRateAIC, oneRateAIC)
   write.csv(AICs, sprintf("ana/AICs/Group AICs for %s Reaches.csv", group), row.names = TRUE, quote = FALSE)
   
