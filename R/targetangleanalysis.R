134dc2ef1c620076376dc2905d27ebd119574ac8
Jenn Ruttle <jennruttle@gmail.com>
2020-09-16 18:55:26 -0400

    investigating target angles

--- R/targetangleanalysis.R
+++ R/targetangleanalysis.R
@@ -122,3 +122,66 @@ TargetANOVAcombine<- function(data) {
 }
 
 
+passive55<-passive_loc
+passive65<-passive_loc
+passive75<-passive_loc
+passive85<-passive_loc
+passive95<-passive_loc
+passive105<-passive_loc
+passive115<-passive_loc
+passive125<-passive_loc
+
+passive55[pangles != 55]<- NA
+passive65[pangles != 65]<- NA
+passive75[pangles != 75]<- NA
+passive85[pangles != 85]<- NA
+passive95[pangles != 95]<- NA
+passive105[pangles != 105]<- NA
+passive115[pangles != 115]<- NA
+passive125[pangles != 125]<- NA
+
+p60<- cbind(passive55, passive65)
+p80<- cbind(passive75, passive85)
+p100<- cbind(passive95, passive105)
+p120<- cbind(passive115, passive125)
+p55<-getreachesformodel(p60)
+p65<-getreachesformodel(p80)
+p75<-getreachesformodel(p100)
+p85<-getreachesformodel(p120)
+
+
+p55<-getreachesformodel(passive55)
+p65<-getreachesformodel(passive65)
+p75<-getreachesformodel(passive75)
+p85<-getreachesformodel(passive85)
+p95<-getreachesformodel(passive95)
+p105<-getreachesformodel(passive105)
+p115<-getreachesformodel(passive115)
+p125<-getreachesformodel(passive125)
+
+
+
+
+
+
+
+
+svglite(file='passive localizations by target.svg', width=12, height=16, system_fonts=list(sans = "Arial"))
+layout(matrix(c(1,2,3,4,5,6,7,8), nrow=4, byrow=TRUE), heights=c(2,2,2,2))
+plot(x = 1:160, y = p55$meanreaches, type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '55')
+plot( x = 1:160, y = p65$meanreaches, col = 'green', type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '65')
+plot( x = 1:160, y = p75$meanreaches, col = 'red', type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '75')
+plot( x = 1:160, y = p85$meanreaches, col = 'blue', type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '85')
+plot( x = 1:160, y = p95$meanreaches, col = 'orange', type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '95')
+plot( x = 1:160, y = p105$meanreaches, col = 'purple', type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '105')
+plot( x = 1:160, y = p115$meanreaches, col = 'cyan', type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '115')
+plot( x = 1:160, y = p125$meanreaches, col = 'Brown', type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '125')
+dev.off()
+
+svglite(file='passive localizations by reach target.svg', width=16, height=10, system_fonts=list(sans = "Arial"))
+layout(matrix(c(1,2,3,4), nrow=2, byrow=TRUE), heights=c(2,2))
+plot(x = 1:160, y = p55$meanreaches, type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '60')
+plot( x = 1:160, y = p65$meanreaches, col = 'green', type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '80')
+plot( x = 1:160, y = p75$meanreaches, col = 'red', type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '100')
+plot( x = 1:160, y = p85$meanreaches, col = 'blue', type = 'l', ylim = c(-10,20), xlab = "trial", ylab = "Shift in Hand Esimates", main = '120')
+dev.off()
